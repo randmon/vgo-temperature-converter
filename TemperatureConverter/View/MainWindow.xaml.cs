71472915ae1fe2ab4e89acbbe2a89954bc59fed8
randmon <ccmarques98@gmail.com>
2022-02-20 21:45:27 +0100

    grid layout

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -27,21 +27,21 @@ namespace View
 
         private void ConvertToCelsius(object sender, RoutedEventArgs e)
         {
-            ConvertBox(fahrenheitTextBox, celsiusTextBox, x => (x - 32) / 1.8);
+            ConvertBox(fahrenheitTextBox, x => (x - 32) / 1.8);
         }
 
         private void ConvertToFahrenheit(object sender, RoutedEventArgs e)
         {
-            ConvertBox(celsiusTextBox, fahrenheitTextBox, x => x * 1.8 + 32);
+            ConvertBox(celsiusTextBox, x => x * 1.8 + 32);
         }
 
-        private void ConvertBox(TextBox source, TextBox dest, Func<double, double> conversion)
+        private void ConvertBox(TextBox t, Func<double, double> conversion)
         {
             try
             {
-                var val = double.Parse(source.Text);
+                var val = double.Parse(t.Text);
                 var res = Math.Round(conversion(val), 2);
-                dest.Text = res.ToString();
+                t.Text = res.ToString();
             }
             catch (FormatException)
             {
