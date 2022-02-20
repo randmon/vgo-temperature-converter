6b87db08de66e2a2f65a607747cb134b752ca346
randmon <ccmarques98@gmail.com>
2022-02-20 23:24:21 +0100

    ITemperatureScale

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -14,39 +14,6 @@ namespace View
         {
             InitializeComponent();
         }
-
-        /*private void ConvertCelsius()
-        {
-            //fahrenheitTextBox.Text = Convert(celsiusTextBox, x => x * 1.8 + 32);
-            //kelvinTextBox.Text = Convert(celsiusTextBox, x => x + 273.15);
-        }
-
-        private void ConvertFahrenheit()
-        {
-            //celsiusTextBox.Text = Convert(fahrenheitTextBox, x => (x - 32) / 1.8);
-            //kelvinTextBox.Text = Convert(fahrenheitTextBox, x => (x + 459.67) * 5/9);
-        }
-
-        private void ConvertKelvin()
-        {
-            //celsiusTextBox.Text = Convert(kelvinTextBox, x => x - 273.15);
-            //fahrenheitTextBox.Text = Convert(kelvinTextBox, x => x * 9/5 - 459.67);
-        }
-
-        private string Convert(TextBox t, Func<double, double> conversion)
-        {
-            try
-            {
-                var val = double.Parse(t.Text);
-                var res = Math.Round(conversion(val), 2);
-                return res.ToString();
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show(this, "Please insert a numeric value", "", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return "";
-            }
-        }*/
     }
 
     public class CelsiusConverter : IValueConverter
@@ -80,7 +47,7 @@ namespace View
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var fahrenheit = double.Parse((string)value);
+           var fahrenheit = double.Parse((string)value);
             var kelvin = (fahrenheit + 459.67) / 1.8;
 
             return kelvin;
---
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    public interface ITemperatureScale
+    {
+        string Name { get; }
+
+        double ConvertToKelvin(double temperature);
+        double ConvertFromKelvin(double temperature);
+    }
+}
