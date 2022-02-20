841472db30c9663dbbcef1812257a20ece7339a1
randmon <ccmarques98@gmail.com>
2022-02-20 23:08:43 +0100

    fahrenheit binding

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace View
 {
@@ -58,12 +47,6 @@ namespace View
                 return "";
             }
         }*/
-
-        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            double x = slider.Value;
-            fahrenheitTextBox.Text = Math.Round(x * 9 / 5 - 459.67, 2).ToString();
-        }
     }
 
     public class CelsiusConverter : IValueConverter
@@ -80,6 +63,25 @@ namespace View
         {
             var celsius = double.Parse((string)value);
             var kelvin = celsius + 273.15;
+
+            return kelvin;
+        }
+    }
+
+    public class FahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var fahrenheit = kelvin * 1.8 - 459.67;
+
+            return fahrenheit.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fahrenheit = double.Parse((string)value);
+            var kelvin = (fahrenheit + 459.67) / 1.8;
 
             return kelvin;
         }
