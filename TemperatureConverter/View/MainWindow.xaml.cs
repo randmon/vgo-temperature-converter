b170f02830c20a53c443c45abd94edeb4be8c83a
randmon <ccmarques98@gmail.com>
2022-02-21 14:35:38 +0100

    converter-view-model

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,57 +1,17 @@
-﻿using System;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Data;
-using System.Diagnostics;
-using System.ComponentModel;
+﻿using System.Windows;
 
 namespace View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public double temperatureInKelvin;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            this.DataContext = this;
-        }
-
-        public double TemperatureInKelvin
-        {
-            get
-            {
-                return temperatureInKelvin;
-            }
-            set
-            {
-                temperatureInKelvin = value;
-
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-    }
-
-    public class TemperatureConverter : IValueConverter
-    {
-        public ITemperatureScale TemperatureScale { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var temperature = double.Parse((string)value);
-            return this.TemperatureScale.ConvertToKelvin(temperature);
+            this.DataContext = new ConverterViewModel();
         }
     }
 }
---
+++ TemperatureConverter/View/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace View
+{
+    public class ConverterViewModel : INotifyPropertyChanged
+    {
+        public double temperatureInKelvin;
+        public double TemperatureInKelvin
+        {
+            get
+            {
+                return temperatureInKelvin;
+            }
+            set
+            {
+                temperatureInKelvin = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class TemperatureConverter : IValueConverter
+    {
+        public ITemperatureScale TemperatureScale { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var temperature = double.Parse((string)value);
+            return this.TemperatureScale.ConvertToKelvin(temperature);
+        }
+    }
+}
