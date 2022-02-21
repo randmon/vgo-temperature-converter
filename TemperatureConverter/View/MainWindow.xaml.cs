2e2af82382b72872b92d3c4c5132085f7d56a8a3
randmon <ccmarques98@gmail.com>
2022-02-21 14:47:00 +0100

    converter-view-model-fix

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
 
 namespace View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -12,6 +12,23 @@ namespace View
             InitializeComponent();
 
             this.DataContext = new ConverterViewModel();
+        }
+    }
+    
+    public class TemperatureConverter : IValueConverter
+    {
+        public ITemperatureScale TemperatureScale { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var temperature = double.Parse((string)value);
+            return this.TemperatureScale.ConvertToKelvin(temperature);
         }
     }
 }
--- TemperatureConverter/View/ViewModel.cs
+++ TemperatureConverter/View/ViewModel.cs
@@ -24,21 +24,4 @@ namespace View
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
-
-    public class TemperatureConverter : IValueConverter
-    {
-        public ITemperatureScale TemperatureScale { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var temperature = double.Parse((string)value);
-            return this.TemperatureScale.ConvertToKelvin(temperature);
-        }
-    }
 }
