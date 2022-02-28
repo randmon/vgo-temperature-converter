e33910c5dfad246f0a081831ea6b2b19476f81ea
randmon <ccmarques98@gmail.com>
2022-02-28 15:06:42 +0100

    derived subclass

--- TemperatureConverter/Cells/Cell.cs
+++ TemperatureConverter/Cells/Cell.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace Cells
 {
@@ -11,7 +12,7 @@ namespace Cells
             this.contents = initialContents;
         }
 
-        public T Value
+        public virtual T Value
         {
             get { return contents; }
             set
@@ -22,6 +23,37 @@ namespace Cells
             }
         }
 
+        public Cell<U> Derive<U>(Func<T, U> transformer)
+        {
+            return new Derived<T, U>(this, transformer);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Derived<IN, OUT> : Cell<OUT>
+    {
+        private Cell<IN> dependency;
+        private Func<IN, OUT> transformer;
+
+        public Derived(Cell<IN> dependency, Func<IN, OUT> transformer) : base(transformer(dependency.Value))
+        {
+            this.dependency = dependency;
+            this.transformer = transformer;
+
+            this.dependency.PropertyChanged += (sender, args) => base.Value = transformer(dependency.Value);
+        }
+
+        public override OUT Value
+        {
+            get
+            {
+                return base.Value;
+            }
+            set
+            {
+                // Not implemented yet
+            }
+        }
     }
 }
