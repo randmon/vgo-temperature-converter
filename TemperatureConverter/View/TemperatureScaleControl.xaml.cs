71015f5ad824c03292673b78738c2afc4a84b994
randmon <ccmarques98@gmail.com>
2022-02-28 17:59:18 +0100

    temperature scale control

---
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for TemperatureScaleControl.xaml
+    /// </summary>
+    public partial class TemperatureScaleControl : UserControl
+    {
+        public TemperatureScaleControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
